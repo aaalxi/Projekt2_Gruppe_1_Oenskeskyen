a4c1ba5efd037531f470a7569ca2cbf721f7e4ed
aaalxi <aval0001@stud.ek.dk>
2026-04-07 09:44:53 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/Projekt2Gruppe1OenskeskyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe_1_oenskeskyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe1OenskeskyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe1OenskeskyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt2_gruppe_1_oenskeskyen/Projekt2Gruppe1OenskeskyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe_1_oenskeskyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe1OenskeskyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
