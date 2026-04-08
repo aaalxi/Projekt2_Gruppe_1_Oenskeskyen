5b60bb387cbac2c5bb0c7794f935ceebef0a2100
DanielUgueto <daug0001@stud.kea.dk>
2026-04-08 12:16:21 +0200

    opdateret sql script - småting

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -8,8 +8,8 @@ USE oenskeskyen;
 CREATE TABLE IF NOT EXISTS users
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(155) UNIQUE NOT NULL,
-    email      VARCHAR(155) UNIQUE NULL,
+    username   VARCHAR(255) UNIQUE NOT NULL,
+    email      VARCHAR(255) UNIQUE NULL,
     password   VARCHAR(255)        NOT NULL,
     birthday   DATE                NOT NULL,
     created_at TIMESTAMP           NOT NULL DEFAULT NOW()
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS wish_list
     user_id     INT                 NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
 
-    title       VARCHAR(155)        NOT NULL,
-    share_token VARCHAR(155) UNIQUE NULL,
+    title       VARCHAR(255)        NOT NULL,
+    share_token VARCHAR(255) UNIQUE NULL,
     created_at  TIMESTAMP           NOT NULL DEFAULT NOW()
 );
 
@@ -33,11 +33,11 @@ CREATE TABLE IF NOT EXISTS wish
     wish_list_id INT            NOT NULL,
     FOREIGN KEY (wish_list_id) REFERENCES wish_list (id) ON DELETE CASCADE,
 
-    name         VARCHAR(155)   NOT NULL,
+    name         VARCHAR(255)   NOT NULL,
     description  VARCHAR(500)   NULL,
     url          VARCHAR(1000)  NULL,
     price        DECIMAL(10, 2) NULL,
-    priority     INT            NULL,
+    priority     INT NULL CHECK (priority BETWEEN 1 AND 5),
     created_at   TIMESTAMP      NOT NULL DEFAULT NOW()
 );
 
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS reservation
 (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     wish_id     INT       NOT NULL UNIQUE,
-    user_id     INT       NULL,
+    reserved_by_user_id     INT       NULL,
     FOREIGN KEY (wish_id) REFERENCES wish (id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL,
+    FOREIGN KEY (reserved_by_user_id) REFERENCES users (id) ON DELETE SET NULL,
 
     reserved_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
