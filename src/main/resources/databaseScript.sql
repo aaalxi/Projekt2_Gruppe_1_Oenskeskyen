d9f77ff8c59942e2afb2212723bfc08b2da66a34
DanielUgueto <daug0001@stud.kea.dk>
2026-04-08 11:08:20 +0200

    har ændret application.properties
    i skal sætte jeres miljøvariabler op selv lokalt med jeres egne passwords, url og user

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS users
     email      VARCHAR(155) UNIQUE NULL,
     password   VARCHAR(255)        NOT NULL,
     created_at TIMESTAMP           NOT NULL DEFAULT NOW()
-
 );
 
 CREATE TABLE IF NOT EXISTS wish_list
