44cae6ee9d1c4ae744f65218b79d96b6b7465563
DanielUgueto <daug0001@stud.kea.dk>
2026-04-08 09:40:27 +0200

    tilføjet footer og header som fragments samt PageController

---
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.example.projekt2_gruppe_1_oenskeskyen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "index";
+    }
+}
