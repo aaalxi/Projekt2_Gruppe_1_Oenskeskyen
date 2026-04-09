fb5dc9d5338b5859c59b32988dfb05e96f097f9b
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-04-09 12:28:06 +0200

    WishlistController created

--- src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/WishlistController.java
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/WishlistController.java
@@ -1,5 +1,24 @@
 package com.example.projekt2_gruppe_1_oenskeskyen.controller;
 
+import com.example.projekt2_gruppe_1_oenskeskyen.repository.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
 public class WishlistController {
 
+    @Autowired
+    WishlistRepository wishlistRepository;
+
+    @GetMapping("/wishlists")
+    public String wishlistPage(Model model){
+        int userID = 1; // midlertidigt
+        ArrayList<Wishlist> wishlists = wishlistRepository.getWishlistsbyUserID(userID);
+
+        return "wishlists";
+    }
 }
