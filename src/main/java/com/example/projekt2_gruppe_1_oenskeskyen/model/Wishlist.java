e882dd09e6c2426d0e974e2d5ff9fbdb66380323
RwR92 <ruro0001@stud.ek.dk>
2026-04-09 10:39:02 +0200

    Wishlist model klasse.

---
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/model/Wishlist.java
@@ -0,0 +1,60 @@
+package com.example.projekt2_gruppe_1_oenskeskyen.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Wishlist {
+    private int ID;
+    private int userID;
+    private String title;
+    private String shareToken;
+    private LocalDateTime createdAt;
+
+    public Wishlist(int userID, String title, String shareToken) {
+        this.userID = userID;
+        this.title = title;
+        this.shareToken = shareToken;
+    }
+    public Wishlist(){
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getShareToken() {
+        return shareToken;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setShareToken(String shareToken) {
+        this.shareToken = shareToken;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
