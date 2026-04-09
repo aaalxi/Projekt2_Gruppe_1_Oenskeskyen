45797c81c823584acfe67b5f5ac57443ca085816
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-04-09 10:55:57 +0200

    whatever the fuh ts is

---
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/repository/WishlistRepository.java
@@ -0,0 +1,43 @@
+package com.example.projekt2_gruppe_1_oenskeskyen.repository;
+
+import com.example.projekt2_gruppe_1_oenskeskyen.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDate;
+
+@Repository
+public class WishlistRepository {
+
+    @Autowired
+    DataSource dataSource;
+
+    public User getUserbyID(int id){
+        User user = null;
+        String sql = "SELECT * FROM users WHERE id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()){
+                if (resultSet.next()) {
+                    user = new User(
+                            resultSet.getInt("id"),
+                            resultSet.getString("username"),
+                            resultSet.getString("email"),
+                            resultSet.getDate("birthday").toLocalDate(),
+                            resultSet.getTimestamp("created_at").toLocalDateTime()
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+}
