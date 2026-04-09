d9dbdbf9e36431690ef239630304c08712d832c4
RwR92 <ruro0001@stud.ek.dk>
2026-04-09 12:50:00 +0200

    Wishlist service klasse lavet med metoderne createWishlist getAllWishlistsByUserID deleteWishlist (sessionUserID for at sikre at man ikke kan delete en andens liste ved at gætte list id) findWishlistByID findWishlistByShareToken

---
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/service/WishlistService.java
@@ -0,0 +1,44 @@
+package com.example.projekt2_gruppe_1_oenskeskyen.service;
+
+import com.example.projekt2_gruppe_1_oenskeskyen.model.Wishlist;
+import com.example.projekt2_gruppe_1_oenskeskyen.repository.WishlistRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WishlistService {
+
+    @Autowired
+    WishlistRepo wishlistRepo;
+
+    public void createWishlist(Wishlist w) {
+        wishlistRepo.createWishlist(w);
+
+    }
+
+    public List<Wishlist> getAllWishlistsByUserID(int userID) {
+
+        return wishlistRepo.getAllWishlistsByUserID(userID);
+    }
+
+    public void deleteWishlist(int wishlistID, int sessionUserID) {
+        Wishlist wishlist = wishlistRepo.findWishlistByID(wishlistID);
+
+        if(wishlist != null && wishlist.getUserID() == sessionUserID){
+            wishlistRepo.deleteWishlist(wishlistID);
+        }
+
+    }
+
+    public Wishlist findWishlistByID(int ID) {
+        return wishlistRepo.findWishlistByID(ID);
+
+    }
+
+    public Wishlist findWishlistByShareToken(String token) {
+        return wishlistRepo.findWishlistByShareToken(token);
+
+    }
+}
