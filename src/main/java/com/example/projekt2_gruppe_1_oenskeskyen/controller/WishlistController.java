d2187b21ff6338cb5467111f6938ea3a4df09ac0
RwR92 <ruro0001@stud.ek.dk>
2026-04-10 14:18:45 +0200

    Fjernet autorwier wishlistRepo fra WishlistController

--- src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/WishlistController.java
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/WishlistController.java
@@ -1,8 +1,10 @@
 package com.example.projekt2_gruppe_1_oenskeskyen.controller;
 
+import com.example.projekt2_gruppe_1_oenskeskyen.model.User;
 import com.example.projekt2_gruppe_1_oenskeskyen.model.Wishlist;
 import com.example.projekt2_gruppe_1_oenskeskyen.service.WishlistService;
 import com.example.projekt2_gruppe_1_oenskeskyen.repository.WishlistRepo;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WishlistController {
-
-    @Autowired
-    WishlistRepo wishlistRepo;
 
     @Autowired
     WishlistService wishlistService;
@@ -24,7 +24,7 @@ public class WishlistController {
     @GetMapping("/wishlists")
     public String wishlistPage(Model model){
         int userID = 1; // midlertidigt
-        ArrayList<Wishlist> wishlists = wishlistRepo.getWishlistsbyUserID(userID);
+        List<Wishlist> wishlists = wishlistService.getAllWishlistsByUserID(userID);
         model.addAttribute("wishlists", wishlists);
         System.out.println(wishlists.size());
 
@@ -44,9 +44,16 @@ public class WishlistController {
     }
 
     @PostMapping("/wishlists/delete")
-    public String deleteWishlist(@RequestParam("ID") int id) {
-        wishlistService.deleteWishlist(id);
+    public String deleteWishlist(@RequestParam("ID") int id, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        wishlistService.deleteWishlist(id, user.getID());
 
         return "redirect:/wishlists";
     }
+
+    // (User) er et cast — det fortæller Java at det objekt vi henter fra session er af typen User.
+// session.getAttribute("user") returnerer altid Object, fordi session kan gemme alle typer objekter.
+// Java ved derfor ikke hvilken type det er uden at vi fortæller det eksplicit via et cast.
+// Hvis vi ikke caster, kan vi ikke tilgå User-specifikke metoder som getID(), getUsername() osv.
+
 }
