d13b99df27202f881f844784e2c653e3d2952fce
RwR92 <ruro0001@stud.ek.dk>
2026-04-10 09:36:35 +0200

    UserController. lavet og HttpSessions til login og logud for at huske en bruger mellem requests

--- src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/UserController.java
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/controller/UserController.java
@@ -1,4 +1,72 @@
 package com.example.projekt2_gruppe_1_oenskeskyen.controller;
 
+import com.example.projekt2_gruppe_1_oenskeskyen.model.User;
+import com.example.projekt2_gruppe_1_oenskeskyen.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user-register")
+    public String showRegisterUser(){
+        return "register-user";
+    }
+
+    @PostMapping("/user-register")
+    public String registerUser(@RequestParam("username") String username,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password,
+                               @RequestParam("birthday")LocalDate birthday,
+                               HttpSession session){
+
+        User user = new User(username,email,password,birthday);
+        userService.register(user);
+        session.setAttribute("user", user);
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        HttpSession session){
+        User user = userService.login(username, password);
+        if(user != null){
+            session.setAttribute("user", user);
+            return "redirect:/profile";
+
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/login";
+    }
+
+    /*
+    HttpSession er en mekanisme i spring der bruges til at
+    huske data mellem HTTP requests.
+
+    serveren husker ikke hvem man er mellem requests, så hver gang man
+    besøger en ny side er det som om serveren ser en for første gang.
+
+    HttpSession fikser det ved at gemme data på serveren og knytte det til
+    en spesifik user ved et session ID der auto sendes frem og tilbage i en cookie.
+     */
 }
