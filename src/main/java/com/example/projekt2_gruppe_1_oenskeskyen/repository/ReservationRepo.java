c3d49f6c25083de06868b3f6eef817c4c67edb63
RwR92 <ruro0001@stud.ek.dk>
2026-04-16 21:53:52 +0200

    Sat HTML i shared-wishlist.html en form for hvis et ønske ikke er reserveret. en form hvis ønsket er reserveret af brugeren en paragraf hvis ønsket er reserveret af en anden. Det hele tjekkes også med den boolean canReserve som sættes til true i ReservationController hvis user != null && user.getID != wishlist.getUserID

--- src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/repository/ReservationRepo.java
+++ src/main/java/com/example/projekt2_gruppe_1_oenskeskyen/repository/ReservationRepo.java
@@ -3,7 +3,6 @@ package com.example.projekt2_gruppe_1_oenskeskyen.repository;
 import com.example.projekt2_gruppe_1_oenskeskyen.model.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -35,13 +34,13 @@ public class ReservationRepo {
     public Reservation getReservationByWishID(int wishID) {
         String sql = "SELECT * FROM reservation WHERE wish_id = ?";
 
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql)){
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-            statement.setInt(1,wishID);
+            statement.setInt(1, wishID);
             ResultSet resultSet = statement.executeQuery();
 
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 Reservation reservation = new Reservation();
 
                 reservation.setId(resultSet.getInt("id"));
@@ -52,7 +51,7 @@ public class ReservationRepo {
                 return reservation;
             }
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
